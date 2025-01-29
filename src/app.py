fec507e4d299b500ef8118847a428993a6ff1193
Joan <79165234+Joangb85@users.noreply.github.com>
2025-01-29 20:46:18 +0000

    GET calls implemented

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Users,Planets,Films,People,Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,55 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
+@app.route('/users', methods=['GET'])
+def handle_get_users():
+    user_list = Users.query.all()
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "content": user_list
     }
-
     return jsonify(response_body), 200
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def handle_get_user(user_id):
+    user = Users.query.get(user_id)
+    response_body = {
+        "content": user
+    }
+    return jsonify(response_body), 200
+
+@app.route('/planets', methods=['GET'])
+def handle_get_planets():
+    planet_list = Planets.query.all()
+    response_body = {
+        "content": planet_list
+    }
+    return jsonify(response_body), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def handle_get_planet(planet_id):
+    planet = Planets.query.get(planet_id)
+    response_body = {
+        "content": planet
+    }
+    return jsonify(response_body), 200
+
+@app.route('/films', methods=['GET'])
+def handle_get_films():
+    film_list = Films.query.all()
+    response_body = {
+        "content": film_list
+    }
+    return jsonify(response_body), 200
+
+@app.route('/films/<int:film_id>', methods=['GET'])
+def handle_get_film(film_id):
+    film = Films.query.get(film_id)
+    response_body = {
+        "content": film
+    }
+    return jsonify(response_body), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,91 @@
+
 from flask_sqlalchemy import SQLAlchemy
+from dataclasses import dataclass
+from sqlalchemy import ForeignKey
+from enum import Enum
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    
+@dataclass
+class Planets(db.Model):
+    __tablename__ = 'Planets'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    ID:int = db.Column(db.Integer, primary_key=True, unique=True)
+    name:str = db.Column(db.String(250), nullable=False, unique=True)
+    population:str = db.Column(db.String(250), nullable=False)
+    climate:str = db.Column(db.String(250), nullable=False)
+    diameter:str = db.Column(db.String(250), nullable=False)
+    gravity:str = db.Column(db.String(250), nullable=False)
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Planets %r>' % self.planets
 
+@dataclass
+class Films(db.Model):
+    __tablename__ = 'Films'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    ID:int = db.Column(db.Integer, primary_key=True, unique=True)
+    title:str = db.Column(db.String(250), nullable=False, unique=True)
+    episode_id:str = db.Column(db.String(250), nullable=False)
+    release_date:str = db.Column(db.String(250), nullable=False)
+    director:str = db.Column(db.String(250), nullable=False)
+    producer:str = db.Column(db.String(250), nullable=False)
+    def __repr__(self):
+        return '<Films %r>' % self.films
+
+@dataclass    
+class People(db.Model):
+    __tablename__ = 'People'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    ID:int = db.Column(db.Integer, primary_key=True, unique=True)
+    name:str = db.Column(db.String(250), nullable=False, unique=True)
+    species:str = db.Column(db.String(250), nullable=False)
+    skin_color:str = db.Column(db.String(250), nullable=False)
+    hair_color:str = db.Column(db.String(250), nullable=False)
+    height:str = db.Column(db.String(250), nullable=False)
+    homeworld:int = db.Column(db.Integer, ForeignKey(Planets.ID), nullable=False)
+    def __repr__(self):
+        return '<People %r>' % self.people
+
+@dataclass     
+class Users(db.Model):
+    __tablename__ = 'Users'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    ID:int = db.Column(db.Integer, primary_key=True, unique=True)
+    first_name:str = db.Column(db.String(250), nullable=False)
+    last_name:str = db.Column(db.String(250), nullable=False)
+    email:str = db.Column(db.String(250), nullable=False, unique=True)
+    username:str = db.Column(db.String(250), nullable=False, unique=True)
+    def __repr__(self):
+        return '<Users %r>' % self.username
+    
+class FavoriteTypeEnum(Enum):
+    Planet= "Planet"
+    People = "People"
+    Films = "Films"
+
+@dataclass 
+class Favorites(db.Model):
+    __tablename__ = 'Favorites'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    ID:int = db.Column(db.Integer, primary_key=True, unique=True)
+    User_id:int = db.Column(db.Integer, ForeignKey(Users.ID), nullable=False)
+    external_ID:int = db.Column(db.Integer, nullable=False)
+    name:str = db.Column(db.String(250), nullable=False)
+    type:str = db.Column(db.Enum(FavoriteTypeEnum), nullable=False, unique=True)
+    def __repr__(self):
+        return '<Favorites %r>' % self.favorites
+
+"""
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+"""
